1636cacec258614679f9c52b921270ab2ad23963
Timur Brachkow <33203187+brachkow@users.noreply.github.com>
2023-07-20 01:20:32 +0200

    Auto generate type definitions

--- index.d.ts
+++ index.d.ts
@@ -1,10 +1,9 @@
-declare type Provider = {
-  id: string;
-  url: string;
-  label: string;
-  keyword: string;
-};
-
-declare const PROVIDERS: Provider[];
-
-export = PROVIDERS;
+export const PROVIDERS: {
+    id: string;
+    url: string;
+    label: string;
+    keywords: string[];
+}[];
+export function getProvider(id: any): any;
+export default PROVIDERS;
+//# sourceMappingURL=index.d.ts.map
