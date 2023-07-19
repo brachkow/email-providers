d7ed91d783f7582f9846dc1068cd3bfa63ed6a71
Timur Brachkow <33203187+brachkow@users.noreply.github.com>
2023-07-20 01:39:59 +0200

    Better types for getters

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+/**
+ * @typedef {Object} Provider
+ * @property {string} id
+ * @property {string} url — url of web interface
+ * @property {string} label — label that can be outputed into UI
+ * @property {string[]} keywords — keyword that can be used to determine that email belongs to the provider
+ */
+
+/** @constant
+ *  @type {Provider[]}
+ */
 export const PROVIDERS = [
   {
     id: 'GMAIL',
@@ -43,13 +54,23 @@ export const PROVIDERS = [
   },
 ];
 
+/**
+ * Gets provider by id
+ * @param {string} id - id of provider
+ * @returns {(Provider | undefined)} — returns provider object if found, and undefined if not found
+ */
 export const getProvider = (id) =>
   PROVIDERS.find((provider) => provider.id === id); //#FIXME Completely rename to getProviderById in next version
 export const getProviderById = getProvider;
 
-export const getProviderByMatch = (value) =>
+/**
+ * Gets provider from email
+ * @param {string} email - email to search
+ * @returns {(Provider | undefined)} — returns provider object if found, and undefined if not found
+ */
+export const getProviderByEmail = (email) =>
   PROVIDERS.find((provider) => {
-    return provider.keywords.find((keyword) => value.includes(keyword));
+    return provider.keywords.find((keyword) => email.includes(keyword));
   });
 
 export default PROVIDERS;
