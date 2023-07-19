c1eff1c2dda6b4eb00886779a425bdb29d00eb08
Timur Brachkow <33203187+brachkow@users.noreply.github.com>
2023-07-20 01:53:42 +0200

    More getters (#7)
    * Soft rename getProvider to getProviderById
    
    * Add getProviderByMatch
    
    * Better types for getters

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,31 @@
-export const PROVIDERS: {
-    id: string;
-    url: string;
-    label: string;
-    keywords: string[];
-}[];
-export function getProvider(id: any): any;
+/**
+ * @typedef {Object} Provider
+ * @property {string} id
+ * @property {string} url — url of web interface
+ * @property {string} label — label that will can be outputed into UI
+ * @property {string[]} keywords — keyword that can be used to determine that email belongs to the provider
+ */
+/** @constant
+ *  @type {Provider[]}
+ */
+export const PROVIDERS: Provider[];
+export function getProvider(id: string): (Provider | undefined);
+export function getProviderById(id: string): (Provider | undefined);
+export function getProviderByEmail(email: string): (Provider | undefined);
 export default PROVIDERS;
+export type Provider = {
+    id: string;
+    /**
+     * — url of web interface
+     */
+    url: string;
+    /**
+     * — label that will can be outputed into UI
+     */
+    label: string;
+    /**
+     * — keyword that can be used to determine that email belongs to the provider
+     */
+    keywords: string[];
+};
 //# sourceMappingURL=index.d.ts.map
--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+/**
+ * @typedef {Object} Provider
+ * @property {string} id
+ * @property {string} url — url of web interface
+ * @property {string} label — label that can be outputed into UI
+ * @property {string[]} keywords — keyword that can be used to determine that email belongs to the provider
+ */
+
+/** @constant
+ *  @type {Provider[]}
+ */
 export const PROVIDERS = [
   {
     id: 'GMAIL',
@@ -43,6 +54,23 @@ export const PROVIDERS = [
   },
 ];
 
-export const getProvider = (id) => PROVIDERS.find((provider) => provider.id === id);
+/**
+ * Gets provider by id
+ * @param {string} id - id of provider
+ * @returns {(Provider | undefined)} — returns provider object if found, and undefined if not found
+ */
+export const getProvider = (id) =>
+  PROVIDERS.find((provider) => provider.id === id); //#FIXME Completely rename to getProviderById in next version
+export const getProviderById = getProvider;
+
+/**
+ * Gets provider from email
+ * @param {string} email - email to search
+ * @returns {(Provider | undefined)} — returns provider object if found, and undefined if not found
+ */
+export const getProviderByEmail = (email) =>
+  PROVIDERS.find((provider) => {
+    return provider.keywords.find((keyword) => email.includes(keyword));
+  });
 
 export default PROVIDERS;
